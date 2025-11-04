876616c3882bc83443d4d6d296b705f971c721f1
pauverblom <pauver.mola@gmail.com>
2025-11-04 16:47:30 +0100

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,20 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("com.google.devtools.ksp") version "1.9.0-1.0.13"
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp") version "2.2.21-2.0.4"
 }
 
 
 android {
     namespace = "com.metrovalencia.widget"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.metrovalencia.widget"
         minSdk = 34
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -32,18 +34,18 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.6.11"
     }
     packaging {
         resources {
@@ -76,6 +78,10 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     ksp(libs.moshi.kotlin.codegen)
+    // Add this line for Glance App Widgets
+    implementation(libs.androidx.glance.appwidget)
 
+    // Make sure you also have the base glance dependency
+    implementation(libs.androidx.glance)
 
 }
